48c988ea553d853ba30f16c0729859112977ac63
mathiasminacapilli <mathiasminacapilli@gmail.com>
2020-06-08 18:04:05 -0300

    Add basic app structure

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Routes
+app.get('/', (req, res) => res.send('Hello World!'))
+
+// Server
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
